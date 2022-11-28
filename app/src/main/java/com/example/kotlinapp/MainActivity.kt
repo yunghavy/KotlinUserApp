f508b6fad38469b8d9dba8430fcb892e9f5dc747
kimcipher <kimutaij192@gmail.com>
2022-11-28 11:00:52 +0300

    first commit

---
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.kotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+// signup page, login screen
+/* text view -> signup
+4 edit text -> username, email, phone and password
+button -> register
+
+Already registered? Login
+
+-< Login
+TextView -> Login
+2 Edit text -> email, password
+button -> login
+
+Not registered yet? Sign up
+
+Drawables -> design of a component
+
+ */
